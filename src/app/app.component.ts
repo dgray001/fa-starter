3f1962aeebcab10924796a89ae27378a09123869
dgray001 <dgray@raybeam.com>
2022-01-18 12:38:21 -0600

    Made Component skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Chart } from 'chart.js';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+import { OptionsComponent } from './options/options.component';
 
 @Component({
   selector: 'app-root',
