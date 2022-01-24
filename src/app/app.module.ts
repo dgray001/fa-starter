f9c4590d3ad654ae9fa6fb3495772927fb827e5f
dgray001 <dgray@raybeam.com>
2022-01-24 10:04:24 -0600

    Finished autocomplete dropdown lists for input and output format boxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
@@ -19,6 +19,7 @@ import { OptionsComponent } from './options/options.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
     HttpClientModule,
   ],
--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { FORMATS } from '../formatlist';
 
 @Component({
@@ -9,11 +11,37 @@ import { FORMATS } from '../formatlist';
 })
 export class OptionsComponent implements OnInit {
   formats: String[] = FORMATS;
-  //currentList: Observable<String[]> =
+  inputControl = new FormControl('', [Validators.required, validateFormat]);
+  inputList: Observable<String[]> = this.inputControl.valueChanges.pipe(
+    map(value => {
+      return this.formats.filter(format => {
+        return format.toLowerCase().includes(value.toLowerCase())
+          && !format.includes("Write-only");
+      });
+    }),
+    startWith(this.formats)
+  );
+  outputControl = new FormControl('', [Validators.required, validateFormat]);
+  outputList: Observable<String[]> = this.outputControl.valueChanges.pipe(
+    map(value => {
+      return this.formats.filter(format => {
+        return format.toLowerCase().includes(value.toLowerCase())
+          && !format.includes("Read-only");
+      });
+    }),
+    startWith(this.formats)
+  );
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+function validateFormat(control: AbstractControl): {[key: string]: any} | null {
+  if (!FORMATS.includes(control.value)) {
+    return {invalidFormat: true};
+  }
+  return null;
 }
