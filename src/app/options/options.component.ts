898558480d1e11328292d56e4145ac7dc3993e3e
dgray001 <dgray@raybeam.com>
2022-01-26 07:15:24 -0600

    Requested style change

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -32,10 +32,9 @@ export class OptionsComponent implements OnInit {
     }),
   );
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
 
--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -8,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class OutputComponent implements OnInit {
   outputString: string = "";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
