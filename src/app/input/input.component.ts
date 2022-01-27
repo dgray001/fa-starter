d85c440028788dc6f61c1afd1b28053ee8ee56d6
dgray001 <dgray@raybeam.com>
2022-01-27 11:30:09 -0600

    Data service in all components, removed ngOnInit

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent {
   inputString: string = "";
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
+  constructor(private readonly dataService: DataService) {}
 }
