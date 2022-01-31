17088f6a17d6a1cc58e439faf16ae6ad1eba5d8b
dgray001 <dgray@raybeam.com>
2022-01-31 14:03:49 -0600

    imports and declarations for SubmitService

--- src/app/submit.service.ts
+++ src/app/submit.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OpenBabelData } from './OpenBabelData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubmitService {
+  readonly submitUrl: string = "/submit";
+  data: OpenBabelData = {inputString: "", inputFormat: "", outputFormat: "",
+    additionalOptions: ""};
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  submit(): Observable<OpenBabelData> {
+    return this.http.patch<OpenBabelData>(this.submitUrl, this.data);
+  }
 }
