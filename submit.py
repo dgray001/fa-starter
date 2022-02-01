f221769fa7f736fe119ff7c40018ee8456785e09
dgray001 <dgray@raybeam.com>
2022-02-01 17:08:35 -0600

    Trying to make computeengine work

--- submit.py
+++ submit.py
@@ -3,12 +3,19 @@ import os
 import flask
 from flask import request
 
+#import google.cloud_v1 as compute_v1
+
 blueprint = flask.Blueprint('submit', __name__, url_prefix="/submit")
 
 blankData = {"inputString": "input", "inputFormat": "inputFormat", "outputFormat": "outputFormat", "additionalOptions": "flags", "output": "output", "log": ""}
 
+projectId = "personal-fa-starter-app"
+zone = "us-west4-b"
+
 @blueprint.route("/test/", methods=['PATCH'])
 def test():
-    req = request.form
+    req = request.get_json()
     print(req)
-    return blankData
+ #   instance_client = compute_v1.InstanceClient()
+
+    return req
