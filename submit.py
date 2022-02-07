985a0ace659d0459626fcba6d94bd11e119d9055
dgray001 <dgray@raybeam.com>
2022-02-07 10:18:51 -0600

    Trying private connection again

--- submit.py
+++ submit.py
@@ -9,12 +9,10 @@ from base64 import decodebytes
 
 blueprint = flask.Blueprint('submit', __name__, url_prefix="/submit")
 
-ip = "34.125.77.241" # external computeengine ip
-#ip = "10.182.0.2" # internal computeengine ip
-keyPath = "./test"
+ip = "10.168.0.42"
 ssh_protocol = "ssh-rsa"
 projectId = "personal-fa-starter-app"
-zone = "us-west4-b"
+zone = "us-west2-b"
 
 @blueprint.route("/", methods=['PATCH'])
 def submit():
@@ -26,7 +24,7 @@ def submit():
 
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(ip, username="yodan", password="", key_filename=keyPath)
+    ssh_client.connect(ip, username="yodan", password="")
     ssh_client.exec_command("mkdir job")
 
     ftp = ssh_client.open_sftp()
