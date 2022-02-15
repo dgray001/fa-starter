ab2fdc4e3fdf237f674d92d838ba5fb93f2a6544
dgray001 <dgray@raybeam.com>
2022-02-15 10:41:11 -0600

    Can test index page

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,14 +2,27 @@ import os
 import tempfile
 import flask_unittest
 import flask.globals
+from flask.testing import FlaskClient
 from app import build_app
 
 class TestApp(flask_unittest.ClientTestCase):
     app = build_app()
 
+    def setUp(self, client):
+        self.assertTrue(client is not None)
+        self.assertTrue(isinstance(client, FlaskClient))
+
+    def tearDown(self, client):
+        self.assertTrue(client is not None)
+        self.assertTrue(isinstance(client, FlaskClient))
+
+    def test_setup(self, client):
+        pass
+
     def test_index_page(self, client):
-        rv = client.get('/')
+        rv: Response = client.get('/')
         self.assertEqual(rv.status_code, 200)
+        rv.close()
 
 
 if __name__ == '__main__':
